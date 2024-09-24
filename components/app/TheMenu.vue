7564ccdc757ef255f76863080bd7f8efd3ae5fb8
André Nunes <andre@ottocar.co.uk>
2024-09-24 23:42:26 +0100

    feat: add footer and menu links

--- components/app/TheMenu.vue
+++ components/app/TheMenu.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+const localePath = useLocalePath();
 </script>
 
 <template>
-  <nav class="bg-base-200">
+  <nav class="bg-base-200 shadow-lg sticky top-0">
     <div class="navbar container mx-auto">
       <div class="navbar-start">
         <label for="zephyrus-drawer" aria-label="open sidebar" class="btn btn-circle bg-base-300 swap swap-rotate">
@@ -34,9 +34,9 @@
         </label>
       </div>
       <div class="navbar-center">
-        <a class="avatar w-12 h-12">
+        <a :href="localePath('/')" class="avatar w-12 h-12">
           <div class="w-full h-full rounded">
-            <NuxtImg src="img/logo.png" alt="Zephyrus Logo"/>
+            <IconZephyrus class="w-full h-full" />
           </div>
         </a>
       </div>
@@ -54,23 +54,6 @@
                 stroke-width="2"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
-        </button>
-        <button class="btn btn-ghost btn-circle">
-          <div class="indicator">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-              <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-            <span class="badge badge-xs badge-primary indicator-item"></span>
-          </div>
         </button>
       </div>
     </div>
---
+++ composable/useSystem.ts
@@ -0,0 +1,8 @@
+export default function () {
+    return useState('system', () => {
+        return {
+            name: 'ZEPHYRUS PROSPERITY - UNIPESSOAL LDA',
+            foundation: '2024-08-13',
+        };
+    });
+}
